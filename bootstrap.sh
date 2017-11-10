2930d2d0fc77e51295ae6316098c6bef643ab06f
watysay <sebastien.manic@gmail.com>
2017-11-10 21:28:51 +0100

    created bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get update -yq && \
+sudo apt-get upgrade -yq
+
+sudo apt-get install git ansible -yq
+cd $HOME
+git clone https://github.com/watysay/install_nvo_pc.git
+cd install_nvo_pc
+ansible-playbook -i hosts playbook.yml
