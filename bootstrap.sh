de333aa9c4d3faae8509e25389ad2af4dea503a7
watysay <sebastien.manic@gmail.com>
2018-10-26 17:29:05 +0200

    changing few steps for bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+# getting sudo rights
+sudo -v
 
-sudo apt-get update -yq
 sudo apt-get install software-properties-common -yq
 sudo apt-add-repository ppa:ansible/ansible -y
+
+# prolonging sudo rights
+sudo -v
+
+# upgrading
 sudo apt-get update -yq
 sudo apt-get upgrade -yq
 
+# install needed parts for launching ansible
 sudo apt-get install git ansible -yq
+
+#--- preliminary installations ~ 20 min ---
+
 
 cd $HOME
 git clone https://github.com/watysay/install_nvo_pc.git
 cd install_nvo_pc
-ansible-playbook -i localhost, playbook.yml --ask-become-pass
+
+# prolonging sudo rights
+sudo -v
+ansible-playbook -i localhost, playbook.yml
