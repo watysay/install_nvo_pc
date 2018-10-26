2129fd722bf16377720452542599913dbb661ed3
watysay <sebastien.manic@gmail.com>
2018-10-26 16:22:48 +0200

    remove hosts file as it's not needed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-sudo apt-get update -yq 
+sudo apt-get update -yq
 sudo apt-get install software-properties-common -yq
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -yq
@@ -12,4 +12,4 @@ sudo apt-get install git ansible -yq
 cd $HOME
 git clone https://github.com/watysay/install_nvo_pc.git
 cd install_nvo_pc
-ansible-playbook -i hosts playbook.yml --ask-become-pass
+ansible-playbook -i localhost, playbook.yml --ask-become-pass
