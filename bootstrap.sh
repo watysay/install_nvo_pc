4235b44ed77b9dc6d219245d8e0b9357a9215718
Sbastien Manic <smanic@meti.fr>
2021-02-15 19:00:28 +0100

    ajout gestion version d'ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,41 @@
 git_repo="https://github.com/watysay/install_nvo_pc.git"
 install_dir="$HOME/install_nvo_pc"
 
+
+
+get_distro_info(){ 
+  if [[ -e /etc/os-release ]]; then
+    . /etc/os-release
+    rc=$?
+  else
+    rc=1
+  fi
+  return $rc
+}
+
+isUbuntu(){
+  if get_distro_info; then
+    return [[ $ID = "ubuntu" ]]
+  else
+    return 1
+  fi
+}
+
+getUbuntuVersion(){ 
+  get_distro_info \
+    && echo "$VERSION_ID" \
+    || echo ""
+} 
+
 # getting sudo rights
 sudo -v
 
 sudo apt-get install software-properties-common -yq
 # add repo for latest ansible
-sudo apt-add-repository ppa:ansible/ansible -y
+# not working for ubuntu > 20
+[[ getUbuntuVersion = 20* ]] \
+  || sudo apt-add-repository ppa:ansible/ansible -y
+
 # add repo for latest git
 sudo apt-add-repository ppa:git-core/ppa -y
 
